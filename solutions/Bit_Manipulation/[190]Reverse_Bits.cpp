312ab5f9ee97bf4d13d618c37bb574cd4bd5aad2
Freitas <adavidaf2002@gmail.com>
2024-02-12 13:27:43 +0000

    [190]Reverse_Bits

---
+++ solutions/Bit_Manipulation/[190]Reverse_Bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ret = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            ret <<= 1;
+            if (n & 1)
+                ret++;
+            n >>= 1;
+        }
+        return (ret);
+    }
+};
