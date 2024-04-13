d69c902631c619f8decbd0d6cab1171fd29dcf75
Freitas <adavidaf2002@gmail.com>
2024-04-13 15:11:43 +0100

    747.MinCostClimbing

---
+++ solutions/1DDynamic_Programming/746.MinCostClimbing.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost)
+    {
+        int a = cost[0], b = cost[1], n = cost.size();
+        if (n <= 2)
+            return min(a, b);
+        for (int i = 2; i < n; i++)
+        {
+            int c = cost[i] + min(a, b);
+            a = b;
+            b = c;
+        }
+        return min(a, b);
+    }
+};
