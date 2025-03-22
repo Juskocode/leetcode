6427be4243d75df537fcfe1e099fc0ed2412b4f2
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-03-22 00:35:17 +0000

    Time: 222 ms (65.53%), Space: 76.3 MB (63.45%) - LeetHub

---
+++ 1830-count-good-meals/1830-count-good-meals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPairs(vector<int>& v) 
+    {
+        int mod = 1e9 + 7;
+        long res = 0, n = v.size();
+        unordered_map<int, int> mp;
+
+        for (int val : v)
+        {
+            for (int p = 1; p <= (1 << 22); p<<=1)
+                if (mp.count(p - val)) 
+                    res += mp[p - val];
+            mp[val]++;
+        }
+        return res % (int) mod;
+    }
+};
