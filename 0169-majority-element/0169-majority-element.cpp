d3dcb3b6d6677c9e4a5c86b0e9e1f77a0787cbc9
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-03-27 21:08:41 +0000

    Time: 1030 ms (6.01%), Space: 28.2 MB (58.44%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int max_freq = 1, elem = nums[0], curr_freq = 1;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != elem)
+                curr_freq--;
+            else
+                curr_freq++;
+            if (curr_freq == 0)
+            {
+                curr_freq = max_freq;
+                elem = nums[i];
+            }
+            max_freq = max(curr_freq, max_freq);
+            cout << curr_freq << " " << max_freq << " " << elem << endl;
+        }
+        return elem;
+    }
+};
