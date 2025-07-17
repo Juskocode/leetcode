0d021056cfa25670f741440b29384d29a21fcd3f
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-07-18 00:10:46 +0100

    Time: 299 ms (23.51%), Space: 66.7 MB (89.67%) - LeetHub

--- 3071-drop-duplicate-rows/3071-drop-duplicate-rows.py
+++ 3071-drop-duplicate-rows/3071-drop-duplicate-rows.py
@@ -1,14 +1,6 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    unique = set()
-    dup = []
-    for id_, email in zip(customers['customer_id'], customers['email']):
-        if email in unique:
-            dup.append(id_ - 1)
-        unique.add(email)
-
-    customers = customers.drop(dup)
-    return customers
+    return customers.drop_duplicates(subset='email', keep='first')
 
     
