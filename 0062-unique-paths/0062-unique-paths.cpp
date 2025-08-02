7530565f40c24af251e5c6b2265ee9d5a1803b45
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-08-02 19:31:20 +0100

    Time: 0 ms (100%), Space: 7.9 MB (87.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int dp[101][101]{};
-    int uniquePaths(int m, int n, int i = 0, int j = 0) {
+    int dfs(int m, int n, int i, int j)
+    {
         if (i >= m || j >= n || i < 0 || j < 0) return 0;
         if (i == m - 1 && j == n - 1) return 1;
         if (dp[i][j]) return dp[i][j];
-        return dp[i][j] = uniquePaths(m, n, i + 1, j) + uniquePaths(m, n, i, j + 1);
+        return dp[i][j] = dfs(m, n, i + 1, j) + dfs(m, n, i, j + 1);
+    }
+    int uniquePaths(int m, int n) {
+        return dfs(m, n, 0, 0);
     }
 };
