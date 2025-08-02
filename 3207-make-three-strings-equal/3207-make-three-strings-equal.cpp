42314bbb60ab21bf0791e5cb254d1a3892915e12
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-08-02 20:20:12 +0100

    Time: 0 ms (100%), Space: 11.7 MB (35.71%) - LeetHub

--- 3207-make-three-strings-equal/3207-make-three-strings-equal.cpp
+++ 3207-make-three-strings-equal/3207-make-three-strings-equal.cpp
@@ -3,16 +3,7 @@ public:
     int findMinimumOperations(string s1, string s2, string s3) {
         int i = 0;
         unsigned total = s1.size() + s2.size() + s3.size();
-        while (s1[i] == s2[i] && s2[i] == s3[i])
-        {
-            i++;
-            if (!s1[i] || s1[i] != s2[i] || s1[i] != s3[i])
-                return total - 3 * i;
-            if (!s2[i] || s2[i] != s3[i] || s2[i] != s1[i])
-                return total - 3 * i;
-            if (!s3[i] || s3[i] != s2[i] || s3[i] != s2[i])
-                return total - 3 * i;
-        }
-        return -1;
+        while (s1[i] && s2[i] && s3[i] && s1[i] == s2[i] && s2[i] == s3[i]) i++;
+        return (i == 0) ? -1 : total - 3 * i;
     }
 };
