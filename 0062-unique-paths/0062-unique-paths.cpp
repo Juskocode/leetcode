804da6d92ba49d0dcce3f003b7971f3ecd2cad92
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-08-02 19:38:06 +0100

    Time: 0 ms (100%), Space: 7.6 MB (100%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,35 @@
+#define ll long long
 class Solution {
 public:
-    int dp[101][101]{};
-    int dfs(int m, int n, int i, int j)
-    {
-        if (i >= m || j >= n || i < 0 || j < 0) return 0;
-        if (i == m - 1 && j == n - 1) return 1;
-        if (dp[i][j]) return dp[i][j];
-        return dp[i][j] = dfs(m, n, i + 1, j) + dfs(m, n, i, j + 1);
+    const ll mod = 2000000033;
+    
+    int modInv(ll x, ll y){
+        x = x % mod;
+        if ( y == 0) return 1; 
+        
+        ll res = 1;
+        if ( y & 1){
+            res = x; 
+        }
+        ll temp = modInv(x, y/2);
+        ll temp2 = (temp * temp ) % mod; 
+        res = ( res * temp2) % mod; 
+        return res; 
     }
+
     int uniquePaths(int m, int n) {
-        return dfs(m, n, 0, 0);
+        
+        vector<ll> fact(205); 
+        fact[0] = fact[1] = 1;
+        for(ll i = 2; i<=201; ++i){
+            fact[i] = ( i * fact[i-1] ) % mod; 
+        }
+
+        ll a = fact[m + n - 2]; 
+        ll b = fact[n-1]; 
+        ll c = fact[m-1]; 
+        ll d = (b * c) % mod; 
+
+        return (a * modInv(d, mod-2)) % mod;
     }
 };
