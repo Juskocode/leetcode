aaefe1c27e9a43ec55b045f46c9d4efb4d9b331d
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-08-10 19:10:01 +0100

    Time: 557 ms (5.04%), Space: 62.1 MB (52.46%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& v, int k) {
+        unsigned n = v.size(), l = 0, r = n - 1, count = 0;
+        sort(v.begin(), v.end());
+
+        while (l < r)
+        {
+            int cur = v[l] + v[r];
+            cout << l << " " << r << " " << v[l] << " " << v[r] << endl;
+            if (cur == k)
+            {
+                count++;
+                l++;
+                r--;
+            }
+            else if (cur < k)
+                l++;
+            else
+                r--;
+        }
+        return count;
+    }
+};
