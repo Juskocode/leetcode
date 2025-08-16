c76ad33868821e57a213ed58562e4276ea8386b0
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-08-16 04:05:45 +0100

    Time: 735 ms (8.58%), Space: 700.3 MB (5.03%) - LeetHub

---
+++ 1397-search-suggestions-system/1397-search-suggestions-system.cpp
@@ -0,0 +1,99 @@
+struct TrieNode {
+    TrieNode* next[26];
+    bool end = false;
+    bool vis = false;
+};
+
+class Trie {
+    public:
+        
+        TrieNode* root;
+
+        Trie()
+        {
+            root = new TrieNode();
+        }
+
+        bool operator < (const Trie &lhs)
+        {
+            return true;
+        }
+
+        void insert(const string &word)
+        {
+            TrieNode* cur = root;
+            for (auto const & c : word)
+            {
+                if (!cur->next[c - 'a'])
+                    cur->next[c - 'a'] = new TrieNode();
+                cur = cur->next[c - 'a'];
+            }
+            cur->end = true;
+        }
+
+        bool search(const string &word)
+        {
+            TrieNode* cur = root;
+            for (auto const & c : word)
+            {
+                if (!cur->next[c - 'a'])
+                    return false;
+                cur = cur->next[c - 'a'];
+            }
+            return cur->end;
+        }
+
+        vector<string> startsWithK(const string &prefix, unsigned k)
+        {
+            vector<string> matches;
+            TrieNode* cur = root;
+            for (auto const & c : prefix) {
+                if (!cur->next[c - 'a'])
+                    return {};
+                cur = cur->next[c - 'a'];
+            }
+
+            using Entry = pair<string, TrieNode*>;
+            struct Cmp {
+                bool operator()(Entry const& a, Entry const& b) const {
+                    return a.first > b.first;
+                }
+            };
+
+            priority_queue<Entry, vector<Entry>, Cmp> q;
+            q.push({prefix, cur});
+
+            while (!q.empty()) {
+                auto [val, u] = q.top();q.pop();
+                //cout << val << " " << u->end << endl;
+                if (u->end) {
+                    matches.push_back(val);
+                    if (matches.size() == k) return matches;
+                }
+
+                for (int i = 0; i < 26; i++)
+                    if (u->next[i])
+                        q.push({val + char(i + 'a'), u->next[i]});
+            }
+            return matches;
+        }
+};
+
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        vector<vector<string>> res;
+        Trie *t = new Trie();
+
+        for (const auto & product : products)
+            t->insert(product);
+        
+        string prefix = "";
+        for (const char & c : searchWord) {
+            prefix += c;
+            res.push_back(t->startsWithK(prefix, 3));
+        }
+        
+        return res;
+    }
+};
