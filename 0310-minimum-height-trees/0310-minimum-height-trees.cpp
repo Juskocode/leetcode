472507b3e52e53bdd284ae437cd31cf91a9d2a90
André Freitas <94986369+Juskocode@users.noreply.github.com>
2025-12-13 10:58:50 +0000

    Time: 11 ms (99.18%), Space: 45.8 MB (99.83%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> graph(n);
-        vector<int> indegree(n, 0), ans; //vector<int> indegree keeps count of the number of nodes approaching a node
-        
-        for(auto &e : edges){   //Creating an adjacency matrix for the given graph
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
-            indegree[e[0]]++;
-            indegree[e[1]]++;
+        if (n==1) return {0};
+
+        vector<int> grp(n), idg(n);
+
+        for (auto& e : edges) {
+            grp[e[0]] ^= e[1];
+            grp[e[1]] ^= e[0];
+            idg[e[0]]++; idg[e[1]]++;
         }
-        
+
         queue<int> q;
-        for(int i=0; i<n;i++){
-            if(indegree[i]==1) q.push(i), indegree[i]--; //add all the leaf nodes to the queue
-        } 
         
-        while(!q.empty()){
-            int s = q.size();
-            ans.clear();
-            for(int i=0; i<s;i++){
-                int curr = q.front(); q.pop();
-                ans.push_back(curr);
-                for(auto child : graph[curr]){ 
-                    indegree[child]--;
-                    if(indegree[child]==1) q.push(child);   
-                }
+        for (int i = 0; i<n; i++) {
+            if (idg[i]==1) q.emplace(i);
+        }
+
+        while(n > 2) {
+            int size = q.size();
+            n -=size;
+            
+            while(size--){
+                int x = q.front(); q.pop();
+                int nei = grp[x];
+                grp[nei] ^= x;
+                if (--idg[nei] == 1) q.emplace(nei);
             }
         }
-        if(n==1) ans.push_back(0); //If there is only 1 node in the graph, the min height is 0, with root being '0'
-        return ans;
-        
+        vector<int> res;
+        while(!q.empty()) {
+            res.emplace_back(q.front());q.pop();
+        }
+
+        return res;
     }
 };
