225ea822ab57d2a0179c5c7fb6557b84e10b3993
André Freitas <94986369+Juskocode@users.noreply.github.com>
2026-02-25 14:35:02 +0000

    Time: 0 ms (100%), Space: 7.8 MB (99.95%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int last = -1, ans = 0;
+        for (int i = 0; i < 32; ++i)
+            if (((n >> i) & 1)) {
+                if (last >= 0)
+                    ans = max(ans, i - last);
+                last = i;
+            }
+
+        return ans;
+    }
+};
