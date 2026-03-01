febc4d4108e41f9c17e17bf20bf8bade98bbe02a
André Freitas <94986369+Juskocode@users.noreply.github.com>
2026-03-01 03:32:51 +0000

    Time: 0 ms (100%), Space: 8 MB (5.21%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        cout << (1431655765 >> (int)(31 - log2(n))) << endl;
         return n == (1431655765 >> (int)(31 - log2(n))) || n <= 2;
     }
 };
