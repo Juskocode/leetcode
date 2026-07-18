14328480d022592c85b374206f1cd815dcc4e0f0
André Freitas <94986369+Juskocode@users.noreply.github.com>
2026-07-18 14:31:53 +0100

    Time: 244 ms (43.3%), Space: 275.9 MB (62.59%) - LeetHub

--- 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
+++ 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
@@ -1,71 +1,57 @@
 class Solution {
 public:
-    int shortestPath(
-        int n,
-        vector<vector<int>>& edges,
-        string labels,
-        int k
-    ) {
+    // [0] -> [1, 1], [2, 3]
+    // [1] -> [2, 1]
+    using State = tuple<int, int, int>;
+    
+    int shortestPath(int n, vector<vector<int>>& edges, string labels, int k) {
         vector<vector<pair<int, int>>> adj(n);
+        string labels_path;
+        int cur_k{};
 
-        for (const auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int weight = edge[2];
+        for (int i = 0; i < edges.size(); i++)
+            adj[edges[i][0]].emplace_back(edges[i][2], edges[i][1]); //u -> w, v
 
-            adj[u].emplace_back(weight, v);
+        priority_queue<State, vector<State>, greater<State>> pq;
+        vector<vector<int>> dist(n, vector<int>(k + 1, INT_MAX));
 
-            // Add this if the graph is undirected:
-            // adj[v].emplace_back(weight, u);
-        }
-
-        using State = tuple<int, int, int>;
-        priority_queue<
-            State,
-            vector<State>,
-            greater<State>
-        > pq;
-
-        const int INF = INT_MAX;
-        vector<vector<int>> dist(n, vector<int>(k + 1, INF));
-
-        // Starting node contributes one character.
         dist[0][1] = 0;
         pq.emplace(0, 0, 1);
 
         while (!pq.empty()) {
-            auto [distance, u, cur_k] = pq.top();
-            pq.pop();
+            auto [w, u, cur_k] = pq.top();pq.pop();
 
-            if (distance != dist[u][cur_k]) {
-                continue;
-            }
+            for (const auto &e: adj[u]) {
+                auto [dw, v] = e;
 
-            for (auto [weight, v] : adj[u]) {
-                int next_k =
-                    (labels[u] == labels[v])
-                    ? cur_k + 1
-                    : 1;
-
-                if (next_k > k) {
-                    continue;
-                }
-
-                int next_distance = distance + weight;
-
-                if (next_distance < dist[v][next_k]) {
-                    dist[v][next_k] = next_distance;
-                    pq.emplace(next_distance, v, next_k);
+                int next_k = (labels[u] == labels[v]) ? (cur_k + 1) : (1);
+                if (next_k <= k && w + dw < dist[v][next_k]) {
+                    dist[v][next_k] = w + dw;
+                    pq.emplace(w + dw, v, next_k);
                 }
             }
         }
-
-        int answer = INF;
+        int answer = INT_MAX;
 
         for (int count = 1; count <= k; count++) {
             answer = min(answer, dist[n - 1][count]);
         }
 
-        return answer == INF ? -1 : answer;
+        return answer == INT_MAX ? -1 : answer;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
